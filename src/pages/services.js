bd9f673f5dc7c766b47ec3921062d012b0529563
Unknown <emmanuel_bernalv@hotmail.com>
2018-09-19 01:53:43 -0500

    Section services
    Create section services

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,33 +1,107 @@
 import React from 'react'
+import rehabilitacion from '../assets/img/rehabilitar.png'
+import instalaciones from '../assets/img/instalaciones_especiales.png'
+import obra from '../assets/img/obra_publica.png'
+import planeacion from '../assets/img/planeacion.png'
+import estructura from '../assets/img/estructuras_acero.png'
+import diseño from '../assets/img/diseño.png'
+import equipo from '../assets/img/equipo_propio.png'
+import supervision from '../assets/img/supervision.png'
 
 const Sevices = () => (
-  <div className='section'>
-    <div className='columns'>
-      <div className='column'>
-        Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.
-        Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500
+  <div className='section services'>
+    <p className='title' align='center'>Nuestros Servicios</p>
+    <br/>
+    <div className='tile is-ancestor'>
+      <div className='tile is-parent is-vertical'>
+        <div className='tile is-child' align='center'>
+          <img className='service-img' src={rehabilitacion} alt="Rehabilitación integral de edificios."/>
+          <p className='title'>Rehabilitación integral de edificios.</p>
+          <p className='subtitle'>
+            Somos especialistas en técnicas de construcción, rehabilitación, restauración y mantenimiento 
+            de edificios en general. Ejecutamos cualquier intervención que requiera el edificio para dejarlo 
+            en perfecto estado, respetando en todo momento la normatividad vigente.
+          </p>
+        </div>
+        <div className='tile is-child' align='center'>
+          <img className='service-img' src={instalaciones} alt="Instalaciones especiales."/>
+          <p className='title'>Instalaciones especiales.</p>
+          <p className='subtitle'>
+            La intervención y especialización de nuestro personal técnico nos permite llevar a cabo 
+            instalaciones eléctricas, hidro-mecánicas,  voz y datos, gases medicinales, aire acondicionado 
+            entre otras; con apego a las normas nacionales e internacionales que rigen para cada tipo de inmueble.
+          </p>
+        </div>
       </div>
-      <div className='column'>
-        Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.
-        Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500
+      <div className='tile is-parent is-vertical'>
+        <div className='tile is-child' align='center'>
+          <img className='service-img' src={obra} alt="Obra Pública."/>
+          <p className='title'>Obra Pública.</p>
+          <p className='subtitle'>
+            Disponemos de un equipo de trabajo profesional, con la experiencia en cualquier rubro de la obra 
+            pública. Siendo responsables de la gestión interna, planeación, desarrollo y ejecución de cada 
+            compromiso adquirido con los gobiernos Municipales y Estatales.
+          </p>
+        </div>
+        <div className='tile is-child' align='center'>
+          <img className='service-img' src={planeacion} alt="Planeación."/>
+          <p className='title'>Planeación.</p>
+          <p className='subtitle'>
+            <ul>
+              <li>Estudios socioeconómicos.</li>
+              <li>Proyectos de Inversión económica.</li>
+              <li>Presupuestos y/o concursos.</li>
+            </ul>
+          </p>
+        </div>
       </div>
-      <div className='column'>
-        Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.
-        Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500
+      <div className='tile is-parent is-vertical'>
+        <div className='tile is-child' align='center'>
+          <img className='service-img' src={estructura} alt="Estructuras de Acero."/>
+          <p className='title'>Estructuras de Acero.</p>
+          <p className='subtitle'>
+            Poseemos los medios necesarios para el diseño y construcción de estructuras metálicas. Al contar 
+            con mano de obra calificada y técnicos enfocados a la optimización de procesos constructivos, 
+            nos permiten reducir tiempos de ejecución de obra, con una gran calidad en nuestros trabajos.
+          </p>
+        </div>
+        <div className='tile is-child' align='center'>
+          <img className='service-img' src={diseño} alt="Diseño."/>
+          <p className='title'>Diseño.</p>
+          <p className='subtitle'>
+            <ul>
+              <li>Proyectos Arquitectónicos.</li>
+              <li>Ingenierías básicas y espaciales.</li>
+              <li>Estudios de imagen urbana.</li>
+              <li>Cálculo de estructuras.</li>
+            </ul>
+          </p>
+        </div>
       </div>
-    </div>
-    <div className='columns'>
-      <div className='column'>
-        Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.
-        Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500
-      </div>
-      <div className='column'>
-        Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.
-        Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500
-      </div>
-      <div className='column'>
-        Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.
-        Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500
+      <div className='tile is-parent is-vertical'>
+        <div className='tile is-child' align='center'>
+          <img className='service-img' src={equipo} alt="Equipo propio del inmueble."/>
+          <p className='title'>Equipo propio del inmueble.</p>
+          <p className='subtitle'>
+          Realizamos la obra civil necesaria, así como el suministro y colocación de equipos especiales, 
+          tales como plantas tratadoras de aguas residuales, cárcamos de bombeo y tanques de almacenamiento 
+          de agua.
+          </p>
+        </div>
+        <div className='tile is-child' align='center'>
+          <img className='service-img' src={supervision} alt="Supervisión."/>
+          <p className='title'>Supervisión.</p>
+          <p className='subtitle'>
+            <ul>
+              <li>Control y seguimiento de obra.</li>
+              <li>Instalaciones especiales.</li>
+              <li>Guías mecánicas.</li>
+              <li>Control de calidad.</li>
+              <li>Control financiero.</li>
+              <li>Control de programas y tiempos de ejecución.</li>
+            </ul>
+          </p>
+        </div>
       </div>
     </div>
   </div>
