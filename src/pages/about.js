3968976d6d1fb3c74d640c0cfc3554a893f2c6bb
Unknown <emmanuel_bernalv@hotmail.com>
2018-09-26 19:53:21 -0500

    fix-about-section

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,24 +2,26 @@ import React from 'react'
 import about from '../assets/img/about_us.jpeg'
 
 const About = () => (
-  <div className='about-section section'>
-    <div className='about-section-resume'>
-      <div className='container is-fluid'>
-        <p className='title'>Sobre nosotros</p>
-        <p>
-          Es una empresa joven integrada por un grupo de profesionales,
-          con una amplia experiencia en el rubro de la <b>construcción, restauración,
-          rehabilitación y mantenimiento,</b> logrando ofrecer  a sus usuarios,
-          una solución eficaz y de calidad en  sus necesidades constructivas.
-          <br /><br />
-          Poseemos la capacidad de trabajar en espacios de gran altura, abiertos y cerrados.
-          <br />
-          Nuestro personal esta capacitado bajo las más altas Normas de Seguridad.
-        </p>
+  <div className=''>
+    <div className='columns is-gapless about-section'>
+      <div className='column'>
+        <div className='section container is-fluid'>
+          <p className='title'>Sobre nosotros</p>
+          <p>
+            Es una empresa joven integrada por un grupo de profesionales,
+            con una amplia experiencia en el rubro de la <b>construcción, restauración,
+            rehabilitación y mantenimiento,</b> logrando ofrecer  a sus usuarios,
+            una solución eficaz y de calidad en  sus necesidades constructivas.
+            <br /><br />
+            Poseemos la capacidad de trabajar en espacios de gran altura, abiertos y cerrados.
+            <br />
+            Nuestro personal esta capacitado bajo las más altas Normas de Seguridad.
+          </p>
+        </div>
       </div>
-    </div>
-    <div className='about-section-resume-img'>
-      <img src={about} alt='Sobre nosotros' />
+      <div className='column'>
+        <div className='about-img'></div>
+      </div>
     </div>
   </div>
 )
